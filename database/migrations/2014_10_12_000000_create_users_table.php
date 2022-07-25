0d344fa96746fefc0d52b1312ef59a13eae6e6a2
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-07-25 16:05:26 +0200

    change users table's primary key

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid();
+            $table->bigIncrements('id');
+            $table->index(['id']);
+            $table->dropPrimary('id');
+            $table->primary(['uuid']);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin')->default(false);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->foreignUuid('avatar')->nullable()->references('uuid')->on('files');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->boolean('is_marketing')->default(false);
+            $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
