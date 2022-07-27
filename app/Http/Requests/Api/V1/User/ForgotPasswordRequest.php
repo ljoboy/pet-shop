66c454dc7d611b392e590f1edc68b902a92a1ebb
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-07-27 22:05:12 +0200

    Password Reset models & co

---
+++ app/Http/Requests/Api/V1/User/ForgotPasswordRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Api\V1\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use JetBrains\PhpStorm\ArrayShape;
+
+final class ForgotPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, array<string>>
+     */
+    #[ArrayShape(['email' => "string[]"])]
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email']
+        ];
+    }
+}
