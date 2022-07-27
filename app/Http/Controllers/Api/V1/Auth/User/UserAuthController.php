9e08998df63968ea9845fc7426d7b60d957f0ca0
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-07-27 21:33:26 +0200

    forgotten password methods out of user controller

--- app/Http/Controllers/Api/V1/Auth/User/UserAuthController.php
+++ app/Http/Controllers/Api/V1/Auth/User/UserAuthController.php
@@ -5,25 +5,18 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api\V1\Auth\User;
 
 use App\Http\Controllers\Api\V1\Auth\AuthController;
+use App\Http\Requests\Api\V1\User\ResetPasswordRequest;
 use App\Http\Requests\Api\V1\User\UserAuthRequest;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
+use Str;
+use Symfony\Component\HttpFoundation\Response;
 
 final class UserAuthController extends AuthController
 {
     public function login(UserAuthRequest $request): JsonResponse
     {
         return $this->loginAttempt($request->only(['email', 'password']), false);
-    }
-
-    public function forgotPassword()
-    {
-
-    }
-
-    public function resetPassword()
-    {
-
     }
 
     public function logout(): JsonResponse
