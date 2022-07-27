3b3b8cf3ae65fa2486bd7cac0e09af80b6b5e384
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-07-27 10:40:01 +0200

    Add seeders to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,21 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
-class DatabaseSeeder extends Seeder
+
+final class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-         \App\Models\User::factory(10)->create();
+         $this->call([
+             AdminSeeder::class,
+             UserSeeder::class,
+         ]);
     }
 }
