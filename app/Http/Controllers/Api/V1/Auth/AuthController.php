992a6b5bfcc886397e0d1b8631da0eb758a6edcf
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-07-27 10:29:43 +0200

    add logout

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -1,36 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api\V1\Auth;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 abstract class AuthController
 {
-    protected function loginAttempt(array $credentials, bool $is_admin): JsonResponse
-    {
-        $response = [
-            "success" => 0,
-            "data" => [],
-            "error" => "Failed to authenticate user",
-            "errors" => [],
-            "trace" => [],
-        ];
-        $http_response = HttpResponse::HTTP_UNPROCESSABLE_ENTITY;
-        $credentials['is_admin'] = $is_admin;
-
-        if ($token = auth()->attempt($credentials)) {
-            $http_response = HttpResponse::HTTP_OK;
-            $response['data'] = $token;
-            $response['error'] = null;
-            $response['success'] = true;
-        }
-
-        return response()->json($response, $http_response);
-    }
-
     public function logout(): JsonResponse
     {
         if (Auth::check()) {
@@ -38,5 +17,31 @@ abstract class AuthController
         }
 
         return response()->json(['message' => 'User successfully signed out']);
+    }
+
+    /**
+     * @param array $credentials
+     */
+    protected function loginAttempt(array $credentials, bool $is_admin): JsonResponse
+    {
+        $response = [
+            'success' => 0,
+            'data' => [],
+            'error' => 'Failed to authenticate user',
+            'errors' => [],
+            'trace' => [],
+        ];
+        $http_response = HttpResponse::HTTP_UNPROCESSABLE_ENTITY;
+        $credentials['is_admin'] = $is_admin;
+
+        $token = auth()->attempt($credentials);
+        if ($token) {
+            $http_response = HttpResponse::HTTP_OK;
+            $response['data'] = $token;
+            $response['error'] = null;
+            $response['success'] = true;
+        }
+
+        return response()->json($response, $http_response);
     }
 }
