cd3b499bc035f2de07e751ef2f3cdcf21fa4cf65
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-07-28 23:18:51 +0200

    user and admin creation routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\V1\Auth\Admin\AdminAuthController;
 use App\Http\Controllers\Api\V1\Auth\ForgotPasswordController;
 use App\Http\Controllers\Api\V1\Auth\PasswordResetController;
 use App\Http\Controllers\Api\V1\Auth\User\UserAuthController;
+use App\Http\Controllers\Api\V1\UserApiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,15 +24,16 @@ Route::prefix('v1')->group(function () {
         Route::controller(UserAuthController::class)->group(function () {
             Route::post('login', 'login');
             Route::get('logout', 'logout');
-            Route::post('create', 'create');
         });
         Route::post('forgot-password', [ForgotPasswordController::class, 'forgotPassword']);
         Route::post('reset-password-token', [PasswordResetController::class, 'resetPassword']);
+        Route::post('create', [UserApiController::class, 'store']);
     });
-    Route::controller(AdminAuthController::class)->prefix('admin')->group(function () {
-        Route::post('login', 'login');
-        Route::get('logout', 'logout');
-        Route::post('create', 'create');
+    Route::prefix('admin')->group(function () {
+        Route::controller(AdminAuthController::class)->group(function () {
+            Route::post('login', 'login');
+            Route::get('logout', 'logout');
+        });
     });
 
     Route::middleware(['auth:api'])->group(function () {
@@ -43,7 +45,9 @@ Route::prefix('v1')->group(function () {
         });
         // User endpoint
         Route::prefix('user')->group(function () {
+            Route::controller(UserApiController::class)->group(function () {
 
+            });
         });
     });
 });
