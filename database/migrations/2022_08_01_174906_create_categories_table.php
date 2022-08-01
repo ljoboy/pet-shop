d3256a36261a8f02f0acde95f429cc6004c6b6d2
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-01 19:53:47 +0200

    Categories model and --all

---
+++ database/migrations/2022_08_01_174906_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+           $table->uuid();
+           $table->bigIncrements('id');
+           $table->index(['id']);
+           $table->dropPrimary('id');
+           $table->primary(['uuid']);
+           $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
