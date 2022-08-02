4c357b75ba9857615935eea77cda350a6703488c
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-02 15:13:57 +0200

    generate all product stuff

---
+++ app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+final class Product extends MyModel
+{
+    use HasFactory;
+}
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+final class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
