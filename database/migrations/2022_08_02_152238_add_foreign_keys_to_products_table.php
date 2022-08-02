df516ae3fa2e586ec0c7424cf1d0c3bdb91b7f25
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-02 23:20:27 +0200

    add foreign key on product table

---
+++ database/migrations/2022_08_02_152238_add_foreign_keys_to_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignUuid('category_uuid')->references('uuid')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('category_uuid');
+            $table->dropIndex('category_uuid');
+            $table->dropColumn('category_uuid');
+        });
+    }
+};
