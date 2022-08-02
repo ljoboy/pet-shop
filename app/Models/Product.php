5ad46927ef6fbec47e5b83a60d88b4977e3521e8
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-02 23:27:51 +0200

    product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,11 +5,41 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Staudenmeir\EloquentJsonRelations\HasJsonRelationships;
+
 
 /**
  * @mixin IdeHelperProduct
  */
 final class Product extends MyModel
 {
-    use HasFactory;
+    use HasFactory, HasJsonRelationships;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'metadata',
+        'price',
+    ];
+
+    protected $casts = [
+        'metadata' => 'json'
+    ];
+
+    /**
+     * @return BelongsTo<Category, Product>
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_uuid', 'uuid');
+    }
+
+    /**
+     * @return BelongsTo<Brand, Product>
+     */
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class,  'metadata->brand', 'uuid');
+    }
 }
