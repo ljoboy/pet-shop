240bb13cd1bb6e370c7c8f573c689b4b3a7e5032
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-03 22:59:05 +0200

    File policies

--- app/Policies/FilePolicy.php
+++ app/Policies/FilePolicy.php
@@ -7,90 +7,33 @@ namespace App\Policies;
 use App\Models\File;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 final class FilePolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
 
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\File  $file
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User|null $user
+     * @param File $file
+     * @return Response|bool
      */
-    public function view(User $user, File $file)
+    public function view(?User $user, File $file): Response|bool
     {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
-    public function create(User $user)
+    public function create(User $user): Response|bool
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\File  $file
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, File $file)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\File  $file
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, File $file)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\File  $file
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, File $file)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\File  $file
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, File $file)
-    {
-        //
+        return true;
     }
 }
