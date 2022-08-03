c042f2f6849977be6afeef102166c331848caba0
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-03 21:32:10 +0200

    user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -6,6 +6,7 @@ namespace App\Policies;
 
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 final class UserPolicy
 {
@@ -14,82 +15,91 @@ final class UserPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
-    public function viewAny(User $user)
+    public function viewAny(User $user): Response|bool
     {
-        //
+        return $user->is_admin;
     }
 
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
-    public function view(User $user, User $model)
+    public function view(User $user, User $model): Response|bool
     {
-        //
+        return $user->uuid === $model->uuid;
     }
 
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
-    public function create(User $user)
+    public function create(User $user): Response|bool
     {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
-    public function update(User $user, User $model)
+    public function update(User $user, User $model): Response|bool
     {
-        //
+        return $user->uuid === $model->uuid;
     }
 
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
-    public function delete(User $user, User $model)
+    public function delete(User $user, User $model): Response|bool
     {
-        //
+        return $user->uuid === $model->uuid;
     }
 
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
-    public function restore(User $user, User $model)
+    public function restore(User $user, User $model): Response|bool
     {
-        //
+        return $user->is_admin;
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
-    public function forceDelete(User $user, User $model)
+    public function forceDelete(User $user, User $model): Response|bool
     {
-        //
+        return $user->uuid === $model->uuid;
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function logoutAdmin(User $user): bool
+    {
+        return $user->is_admin;
     }
 }
