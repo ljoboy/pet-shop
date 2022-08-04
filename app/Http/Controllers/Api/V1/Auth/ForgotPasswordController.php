7b9d6d6f8605c871b36d01c5417306219c4f4aac
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-04 19:28:25 +0200

    adapt ForgotPasswordController.php to ApiController.php

--- app/Http/Controllers/Api/V1/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Api/V1/Auth/ForgotPasswordController.php
@@ -4,16 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api\V1\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use App\Http\Requests\Api\V1\User\ForgotPasswordRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Password;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
-final class ForgotPasswordController extends Controller
+final class ForgotPasswordController extends ApiController
 {
     /**
      * @param ForgotPasswordRequest $request
@@ -21,28 +19,20 @@ final class ForgotPasswordController extends Controller
      */
     public function forgotPassword(ForgotPasswordRequest $request): JsonResponse
     {
-        $error = 'Invalid email';
-        $response = [];
         $email = $request->post('email');
         $user = User::whereEmail($email)->first();
-        $status_code = HttpResponse::HTTP_NOT_FOUND;
 
         if ($user && $user->is_admin) {
-            $error = 'Admin user cannot be edited';
-            $status_code = HttpResponse::HTTP_BAD_REQUEST;
+            return $this->responseError(error: 'Admin user cannot be edited', code: HttpResponse::HTTP_BAD_REQUEST);
         }
 
         if ($user && !$user->is_admin) {
-            $status_code = HttpResponse::HTTP_OK;
-            $response = ['reset_token' => Password::createToken($user)];
-            $error = null;
+            return $this->responseSuccess(
+                data: ['reset_token' => Password::createToken($user)],
+                code: HttpResponse::HTTP_OK
+            );
         }
 
-        $data = [
-            'error' => $error,
-            'data' => $response
-        ];
-
-        return response()->json($data, $status_code);
+        return $this->responseError(error: 'Invalid email!');
     }
 }
