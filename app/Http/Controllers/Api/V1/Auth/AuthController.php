b4d9d5f3ec5bd5bc67ca59c93c1d94622d664905
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-04 15:25:39 +0200

    adapt authController to ApiController

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api\V1\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
-abstract class AuthController extends Controller
+abstract class AuthController extends ApiController
 {
 
     /**
@@ -22,7 +22,7 @@ abstract class AuthController extends Controller
             Auth::logout();
         }
 
-        return response()->json(['message' => 'User successfully signed out']);
+        return $this->responseSuccess(null);
     }
 
     /**
@@ -32,24 +32,12 @@ abstract class AuthController extends Controller
      */
     protected function loginAttempt(array $credentials, bool $is_admin): JsonResponse
     {
-        $response = [
-            'success' => 0,
-            'data' => [],
-            'error' => 'Failed to authenticate user',
-            'errors' => [],
-            'trace' => [],
-        ];
-        $http_response = HttpResponse::HTTP_UNPROCESSABLE_ENTITY;
         $credentials['is_admin'] = $is_admin;
-
         $token = auth()->attempt($credentials);
         if ($token) {
-            $http_response = HttpResponse::HTTP_OK;
-            $response['data'] = $token;
-            $response['error'] = null;
-            $response['success'] = true;
+            return $this->responseSuccess(['token' => $token]);
         }
 
-        return response()->json($response, $http_response);
+        return $this->responseError("failed to authenticate user");
     }
 }
