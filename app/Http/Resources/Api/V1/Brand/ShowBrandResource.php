4c42ac1239643434e4bdaba50ea9db981b569f7f
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-04 20:09:38 +0200

    add attributes to ShowBrandResource.php

--- app/Http/Resources/Api/V1/Brand/ShowBrandResource.php
+++ app/Http/Resources/Api/V1/Brand/ShowBrandResource.php
@@ -4,18 +4,34 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\Api\V1\Brand;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
 
+/**
+ * @property string $title
+ * @property string $slug
+ * @property string $uuid
+ * @property string $created_at
+ * @property string $update_at
+ */
 final class ShowBrandResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param Request $request
+     * @return array|Arrayable<string, string>|JsonSerializable
      */
-    public function toArray($request)
+    public function toArray($request): array|JsonSerializable|Arrayable
     {
-        return parent::toArray($request);
+        return [
+            "uuid" => $this->uuid,
+            "title" => $this->title,
+            "slug" => $this->slug,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->update_at,
+        ];
     }
 }
