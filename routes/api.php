b7378ce8a60a3ea0a610396a3478fb1b3eb3608b
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-05 19:56:41 +0200

    add name to routes

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,8 @@ use App\Http\Controllers\Api\V1\Auth\UserAuthController;
 use App\Http\Controllers\Api\V1\BrandApiController;
 use App\Http\Controllers\Api\V1\CategoryApiController;
 use App\Http\Controllers\Api\V1\FileApiController;
-use App\Http\Controllers\Api\V1\FileController;
 use App\Http\Controllers\Api\V1\ProductApiController;
 use App\Http\Controllers\Api\V1\UserApiController;
-use App\Http\Middleware\IsAdmin;
-use App\Http\Middleware\IsUser;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,51 +22,48 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::prefix('v1')->group(function () {
-    // Admin endpoints
-    Route::prefix('admin')->group(function () {
-        Route::controller(AdminAuthController::class)->group(function () {
-            Route::post('/login', 'login');
-            Route::get('/logout', 'logout');
-        });
-        Route::controller(AdminApiController::class)->group(function () {
-            Route::post('/create', 'store');
-            Route::get('/user-listing', 'userListing');
-            Route::put('/user-edit/{user}', 'update');
-            Route::delete('/user-delete/{user}', 'destroy');
-        });
+// Admin endpoints
+Route::prefix('admin')->as('admin.')->group(function () {
+    Route::controller(AdminAuthController::class)->group(function () {
+        Route::post('/login', 'login')->name('login');
+        Route::get('/logout', 'logout')->name('logout');
     });
-    // User endpoints
-    Route::prefix('user')->group(function () {
-        Route::controller(UserAuthController::class)->group(function () {
-            Route::post('/login', 'login');
-            Route::get('/logout', 'logout');
-        });
-        Route::post('forgot-password', [ForgotPasswordController::class, 'forgotPassword']);
-        Route::post('reset-password-token', [PasswordResetController::class, 'resetPassword']);
-        Route::controller(UserApiController::class)->group(function () {
-            Route::get('/', 'show');
-            Route::delete('/', 'destroy');
-            Route::put('/edit', 'update');
-            Route::post('/create', 'store');
-        });
+    Route::controller(AdminApiController::class)->group(function () {
+        Route::post('/create', 'store')->name('store');
+        Route::get('/user-listing', 'userListing')->name('index');
+        Route::put('/user-edit/{user}', 'update')->name('update');
+        Route::delete('/user-delete/{user}', 'destroy')->name('destroy');
     });
-    // Brand endpoints
-    Route::get('/brands', [BrandApiController::class, 'index']);
-    Route::post('/brand/create', [BrandApiController::class, 'store']);
-    Route::apiResource('brand', BrandApiController::class)->except(['index', 'store']);
-    // Categorie endpoints
-    Route::get('/categories', [CategoryApiController::class, 'index']);
-    Route::post('/categorie/create', [CategoryApiController::class, 'store']);
-    Route::apiResource('categorie', CategoryApiController::class)->except(['index', 'store']);
-    // Product endpoints
-    Route::get('/products', [ProductApiController::class, 'index']);
-    Route::post('/product/create', [ProductApiController::class, 'store']);
-    Route::apiResource('product', ProductApiController::class)->except(['index', 'store']);
-    // File endpoints
-    Route::controller(FileApiController::class)->prefix('file')->group(function () {
-        Route::post('upload', 'store');
-        Route::get('/{file}', 'show');
+});
+// User endpoints
+Route::prefix('user')->as('user.')->group(function () {
+    Route::controller(UserAuthController::class)->group(function () {
+        Route::post('/login', 'login')->name('login');
+        Route::get('/logout', 'logout')->name('logout');
     });
+    Route::post('forgot-password', [ForgotPasswordController::class, 'forgotPassword'])->name('forgot-password');
+    Route::post('reset-password-token', [PasswordResetController::class, 'resetPassword'])->name('reset-password');
+    Route::controller(UserApiController::class)->group(function () {
+        Route::get('/', 'show')->name('show');
+        Route::delete('/', 'destroy')->name('destroy');
+        Route::put('/edit', 'update')->name('update');
+        Route::post('/create', 'store')->name('store');
+    });
+});
+// Brand endpoints
+Route::get('/brands', [BrandApiController::class, 'index'])->name('brands');
+Route::post('/brand/create', [BrandApiController::class, 'store'])->name('brand.store');
+Route::apiResource('brand', BrandApiController::class)->except(['index', 'store']);
+// Categorie endpoints
+Route::get('/categories', [CategoryApiController::class, 'index'])->name('categories');
+Route::post('/categorie/create', [CategoryApiController::class, 'store'])->name('categorie.store');
+Route::apiResource('categorie', CategoryApiController::class)->except(['index', 'store']);
+// Product endpoints
+Route::get('/products', [ProductApiController::class, 'index'])->name('products');
+Route::post('/product/create', [ProductApiController::class, 'store'])->name('product.store');
+Route::apiResource('product', ProductApiController::class)->except(['index', 'store']);
+// File endpoints
+Route::controller(FileApiController::class)->prefix('file')->as('file.')->group(function () {
+    Route::post('upload', 'store')->name('store');
+    Route::get('/{file}', 'show')->name('show');
 });
