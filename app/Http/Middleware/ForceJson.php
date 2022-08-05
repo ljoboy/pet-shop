b167b329bf14c0443efb52b283fd7df217f77d76
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-05 15:53:33 +0200

    force all request to api to accept json

---
+++ app/Http/Middleware/ForceJson.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+final class ForceJson
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        $request->headers->set('Accept', 'application/json');
+        return $next($request);
+    }
+}
