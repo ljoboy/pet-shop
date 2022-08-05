348a0c3eb0b7503618d45917a9c4ff6b9ddf7008
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-08-05 22:56:44 +0200

    reorder method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,49 +9,14 @@ use Illuminate\Http\JsonResponse;
 abstract class ApiController extends Controller
 {
     /**
-     * @param  mixed  $data
-     * @param  string|null  $error
-     * @param  int  $code
-     * @param  array  $errors
-     * @param  array  $extra
-     * @param  bool  $success
-     * @return JsonResponse
-     */
-    private function response(
-        mixed $data,
-        string $error = null,
-        array $errors = [],
-        int $code = 422,
-        array $extra = [],
-        bool $success = false
-    ): JsonResponse {
-        $response = [
-            'success' => $success ? 1 : 0,
-            'data' => $data,
-            'error' => $error,
-            'errors' => $errors,
-        ];
-
-        if (! $success) {
-            $response['trace'] = $extra;
-        }
-
-        if ($success) {
-            $response['extra'] = $extra;
-        }
-
-        return response()->json($response, $code);
-    }
-
-    /**
-     * @param  string|null  $error
-     * @param  array  $errors
-     * @param  int  $code
-     * @param  array  $trace
+     * @param string|null $error
+     * @param array $errors
+     * @param int $code
+     * @param array $trace
      * @return JsonResponse
      */
     protected function responseError(
-        string $error = null,
+        ?string $error = null,
         array $errors = [],
         int $code = 422,
         array $trace = []
@@ -65,5 +30,40 @@ abstract class ApiController extends Controller
         int $code = 200
     ): JsonResponse {
         return $this->response(data: $data, code: $code, extra: $extra, success: true);
+    }
+
+    /**
+     * @param mixed $data
+     * @param string|null $error
+     * @param int $code
+     * @param array $errors
+     * @param array $extra
+     * @param bool $success
+     * @return JsonResponse
+     */
+    private function response(
+        mixed $data,
+        ?string $error = null,
+        array $errors = [],
+        int $code = 422,
+        array $extra = [],
+        bool $success = false
+    ): JsonResponse {
+        $response = [
+            'success' => $success ? 1 : 0,
+            'data' => $data,
+            'error' => $error,
+            'errors' => $errors,
+        ];
+
+        if (!$success) {
+            $response['trace'] = $extra;
+        }
+
+        if ($success) {
+            $response['extra'] = $extra;
+        }
+
+        return response()->json($response, $code);
     }
 }
